ad9d46a94bbcb7f90261810b6bb6cec28dc0ace7
Pfleger <Pflegerdev@gmail.com>
2023-03-09 08:54:58 -0300

    update

---
+++ lista1/src/lista1/CriaBanco.java
@@ -0,0 +1,31 @@
+package lista1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CriaBanco {
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost/";
+		String usuario = "root";
+		String senha = "aluno";
+
+		try {
+			Connection conexao = DriverManager.getConnection(url,usuario,senha);
+			Statement statement = conexao.createStatement();
+			String bd = "Banco";
+			String criaBanco = "CREATE DATABASE IF NOT EXISTS " + bd;
+			statement.executeUpdate(criaBanco);
+			
+			criaBanco = "USE " + bd;
+			statement.executeUpdate(criaBanco);			
+			
+			String criaTabela = "CREATE TABLE IF NOT EXISTS pessoa " + "cpf BEGINT(12) NOT NULL " + "nome VARCHAR(100) "
+					+ "PRIMARY KEY (cpf))";
+			statement.executeUpdate(criaTabela);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
